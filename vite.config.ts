dd97f1a50deef01a250b6f3adf6e20db1aec7e6a
Your Full Name <asapbuyco@gmail.com>
2026-02-22 22:29:48 +0100

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: './',
+  base: '/',
   server: {
-
     host: true,
     port: 5173,
   },
   build: {
-    sourcemap: false,
+    sourcemap: mode === 'development',
     rollupOptions: {
       output: {
         manualChunks: {
@@ -22,4 +21,4 @@ export default defineConfig({
     },
     chunkSizeWarningLimit: 1000,
   },
-});
+}));
